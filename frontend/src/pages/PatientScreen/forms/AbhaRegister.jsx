30c1561b23443b39a53311c66696067f37e6dbc8
B Rahul <rahul20bollisetty@gmail.com>
2024-03-25 16:41:24 +0530

    Merge branch 'main' of /home/fledlucifer/Desktop/HAD with conflicts.

---
+++ frontend/src/pages/PatientScreen/forms/AbhaRegister.jsx
@@ -0,0 +1,223 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+import axios from "../../../api/axios";
+import encryptData from "../../../utils/encryptData";
+function AbhaRegister() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm();
+
+  const {
+    register: register1,
+    handleSubmit: handleSubmit1,
+    reset: reset1,
+    getValues: getValues1,
+    formState: { errors: errors1 },
+  } = useForm();
+
+  const {
+    register: register2,
+    handleSubmit: handleSubmit2,
+    reset: reset2,
+    getValues: getValues2,
+    formState: { errors: errors2 },
+  } = useForm();
+
+  const axiosPrivate = useAxiosPrivate();
+  const [txn, setTxn] = useState("");
+  const [isMobileOTPreq, setMobileOTP] = useState(false);
+
+  return (
+    <div className="grid grid-cols-2 gap-5 text-[#7B7878] font-medium	text-xl mt-8">
+      <div>
+        <div className="flex flex-col">
+          <p className="mr-48 text-sm">Aadhaar Number</p>
+          <div className="relative flex w-full">
+            <input
+              className="rounded-md pr-32 w-full"
+              type="text"
+              name=""
+              {...register("aadhaar", {
+                required: "Required",
+                pattern: {
+                  value: /^[0-9]+$/,
+                  message: "Only Numbers are allowed",
+                },
+              })}
+            />
+            <button
+              className="!absolute p-1 bg-[#006666] text-white right-1 top-[3px] rounded"
+              onClick={handleSubmit(async () => {
+                const data = {
+                  aadhaar: encryptData(getValues("aadhaar")),
+                };
+                const headers = {
+                  "Content-Type": "application/json",
+                };
+                try {
+                  const resp = await axiosPrivate.post(
+                    "/patient/aadhaarOTPinit",
+                    data,
+                    { headers }
+                  );
+                  console.log(resp);
+                  setTxn(resp.data.txnId);
+                  toast.success(resp.data.message);
+                } catch (err) {
+                  if (err?.response?.data) {
+                    if (err?.response?.data?.loginId)
+                      toast.error(err.response.data.loginId);
+                    else toast.error(err.response.data.error.message);
+                  }
+                }
+              })}
+            >
+              Send OTP
+            </button>
+          </div>
+          <p className="errorMsg">{errors.aadhaar?.message}</p>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex flex-col">
+          <p className="mr-48 text-sm">Mobile</p>
+          <div className="relative flex w-full">
+            <input
+              className="rounded-md pr-32 w-full"
+              type="text"
+              name=""
+              id=""
+              {...register1("mobile", {
+                required: "Required",
+                pattern: {
+                  value: /^[0-9]+$/,
+                  message: "Only Numbers are allowed",
+                },
+              })}
+            />
+            <button
+              className="!absolute p-1 bg-[#006666] text-white right-1 top-[3px] rounded"
+              onClick={handleSubmit1(async () => {
+                const data = {
+                  loginId: encryptData(getValues1("mobile")),
+                  txnId: txn,
+                };
+                setMobileOTP(true);
+                const headers = {
+                  "Content-Type": "application/json",
+                };
+                try {
+                  const resp = await axiosPrivate.post(
+                    "/patient/mobileOTPinit",
+                    data,
+                    { headers }
+                  );
+                  console.log(resp);
+                  setTxn(resp.data.txnId);
+                  toast.success(resp.data.message);
+                } catch (err) {
+                  if (err?.response?.data) {
+                    if (err?.response?.data?.loginId)
+                      toast.error(err.response.data.loginId);
+                    else toast.error(err.response.data.error.message);
+                  }
+                }
+              })}
+            >
+              Send OTP
+            </button>
+          </div>
+          <p className="errorMsg">{errors1.mobile?.message}</p>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col">
+          <p className="mr-48 text-sm">OTP</p>
+          <input
+            className="rounded-md w-72"
+            type="text"
+            name=""
+            id=""
+            {...register2("otp", {
+              required: "Required",
+              pattern: {
+                value: /^[0-9]+$/,
+                message: "Only Numbers are allowed",
+              },
+            })}
+          />
+          <p className="errorMsg">{errors2.otp?.message}</p>
+        </div>
+      </div>
+      <div>
+        <div className="flex h-full items-end justify-center w-72">
+          <button
+            type="submit"
+            className="w-40 p-2 bg-[#006666] text-white rounded-md"
+            onClick={handleSubmit2(async () => {
+              var data = null;
+              var path = null;
+              if (isMobileOTPreq) {
+                data = {
+                  otp: encryptData(getValues2("otp")),
+                  txnId: txn,
+                };
+                path="/patient/mobileOTPverify";
+              } else {
+                data = {
+                  otp: encryptData(getValues2("otp")),
+                  mobileNumber: getValues1("mobile"),
+                  transactionId: txn,
+                };
+                path="/patient/aadharOTPverify";
+              }
+              const headers = {
+                "Content-Type": "application/json",
+              };
+              try {
+                const resp = await axiosPrivate.post(
+                  path,
+                  data,
+                  { headers }
+                );
+                console.log(resp);
+                setTxn(resp.data.txnId);
+                if(resp.data?.isNew)toast.success("Send OTP to mobile to link it to ABHA")
+                console.log(resp);
+                setMobileOTP(false);
+                toast.success(resp.data.message);
+              } catch (err) {
+                if (err?.response?.data) {
+                  if (err?.response?.data?.loginId)
+                    toast.error(err.response.data.loginId);
+                  else toast.error(err.response.data.error.message);
+                }
+              }
+            })}
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col">
+          <p className="text-sm">Select ID</p>
+          <select className="w-full rounded-md" name="bloodGroup" id="">
+            <option value="" disabled>
+              Select ID
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AbhaRegister;
