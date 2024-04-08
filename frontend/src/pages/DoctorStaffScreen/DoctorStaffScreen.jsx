b253ff53039c5d171ad21ebaf4a15b88a9de9672
Siddharth <siddharthpillai2001@gmail.com>
2024-04-08 12:50:03 +0530

    Doctor staff list and add screen

---
+++ frontend/src/pages/DoctorStaffScreen/DoctorStaffScreen.jsx
@@ -0,0 +1,26 @@
+import Sidebar from "../Sidebar";
+import StaffTabs from "./components/StaffTabs";
+
+export const DoctorStaffScreen = () => {
+  return (
+    <div className="flex flex-row w-full">
+      <div className="w-fit">
+        <Sidebar />
+      </div>
+      <div className="basis-full bg-[#F1F5FC] overflow-hidden">
+        <div className="flex flex-col h-full">
+          <div className="h-[64px] w-full pb-16 bg-white"></div>
+          <div className="bg-white grow m-3">
+            <div className="flex flex-col">
+              <div className="">
+                <div className="flex mt-6 mb-8  justify-center">
+                  <StaffTabs />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
