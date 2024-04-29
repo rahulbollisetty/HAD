50d840d4bf674a1082e65d099f940c1dc53a48c3
B Rahul <rahul20bollisetty@gmail.com>
2024-04-29 23:34:31 +0530

    configured rbac

--- frontend/src/pages/PatientScreen/components/PastRecords.jsx
+++ frontend/src/pages/PatientScreen/components/PastRecords.jsx
@@ -86,54 +86,58 @@ function PastRecords({ patientId, sendDataToParent }) {
               <th scope="col" className="px-6 py-3">
                 Details
               </th>
-              <th scope="col" className="px-6 py-3"></th>
-              <th scope="col" className="px-6 py-3"></th>
+              <th scope="col" className="px-6 py-3">
+                View
+              </th>
             </tr>
           </thead>
           <tbody className="text-sm text-[#444]">
-            {AppointmentDetailsList.map((item, index) => (
-              <tr className="bg-white border " key={item.appointment_id}>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-[#444] whitespace-nowrap"
-                >
-                  {item.doctor_name}
-                </th>
-                <td className="px-6 py-4">{item.date}</td>
-                <td className="px-6 py-4">{item.time}</td>
-                <td className="px-6 py-4">{item.status}</td>
-                <td className="px-6 py-4">{item.notes}</td>
-                <td className="px-6 py-4 ">
-                  <button
-                    className="inline-flex justify-center items-center gap-[10px] rounded-lg
-                                        border border-[#787887] bg-[#F5FEF2] text-[20px] text-[#02685A] font-semibold p-2.5"
-                    onClick={() => {
-                      sendDataToParent(
-                        item.appointment_id,
-                        item.status === "Completed"
-                      );
-                    }}
+            {AppointmentDetailsList.length === 0 ? (
+              <>
+                <tr>
+                  <td
+                    colSpan={5}
+                    className="text-center text-zinc-600 bg-gray-200 p-6 rounded-bottom-lg font-bold text-[20px]"
                   >
-                    <div>View</div>
-                    <FaCaretRight className="h-[25px] w-[25px]" />
-                  </button>
-                </td>
-                <td className="px-6 py-4">
-                  <button
-                    className="inline-flex justify-center items-center gap-[10px] text-[30px]
-                                        text-[#b82d2d] "
-                    onClick={() => {
-                      deleteAppointment(item.appointment_id);
-                    }}
-                  >
-                    <div>
-                      <MdDelete />
-                      
-                    </div>
-                  </button>
-                </td>
-              </tr>
-            ))}
+                    No records to display
+                  </td>
+                </tr>
+              </>
+            ) : (
+              <>
+                {AppointmentDetailsList.map((item, index) => (
+                  <tr className="bg-white border " key={item.appointment_id}>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-[#444] whitespace-nowrap"
+                    >
+                      {item.doctor_name}
+                    </th>
+                    <td className="px-6 py-4">{item.date}</td>
+                    <td className="px-6 py-4">{item.time}</td>
+                    <td className="px-6 py-4">{item.status}</td>
+                    <td className="px-6 py-4">{item.notes}</td>
+                    <td className="px-6 py-4">
+                      <button
+                        className={role==="STAFF" ?`inline-flex justify-center items-center gap-[10px] rounded-lg
+                         text-[20px] text-white font-semibold p-2.5 bg-gray-500 cursor-not-allowed` :`inline-flex justify-center items-center gap-[10px] rounded-lg
+                                        border border-[#787887] bg-[#F5FEF2] text-[20px] text-[#02685A] font-semibold p-2.5`}
+                                        disabled={role==="STAFF"}
+                        onClick={() => {
+                          sendDataToParent(
+                            item.appointment_id,
+                            item.status === "Completed"
+                          );
+                        }}
+                      >
+                        <div>View</div>
+                        <FaCaretRight className="h-[25px] w-[25px]" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </>
+            )}
           </tbody>
         </table>
       </div>
